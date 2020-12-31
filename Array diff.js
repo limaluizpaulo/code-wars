262c678ab0b1c4d559065d6f33dfa32a37d14623
Luiz Paulo Bernardo de Lima <limaluizpaulo@users.noreply.github.com>
2020-12-31 02:10:32 -0300

    novo exercicio

--- Array diff.js
+++ Array diff.js
@@ -1,17 +1,3 @@
-const a = [-11, 18, -18, -9, -3];
-const b = [-11, -3];
-
-if (b[0] == undefined) {
-  b[0] = -1000;
+function array_diff(a, b) {
+  return a.filter((e) => !b.includes(e));
 }
-
-let novo = [];
-for (const j in a) {
-  for (const i in b) {
-    if (a[j] != b[i]) {
-      novo.push(a[j]);
-    }
-  }
-}
-
-console.log(novo);
